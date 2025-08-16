14d40a4281bc9264d520511cfc798147b1751f65
Batool Kazmi <kazmib6@gmail.com>
2025-08-16 16:31:16 -0400

    added tailwind

--- frontend/cinematch/src/components/NavBar.tsx
+++ frontend/cinematch/src/components/NavBar.tsx
@@ -1,13 +1,10 @@
-import { NavLink, useNavigate, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
   return (
     <>
-      <nav style={{ display: "flex", gap: "1rem" }}>
-        <h1 style={{ marginRight: "2rem" }}>CineMatch</h1>
+      <nav>
+        <h1 className="text-red-700">CineMatch</h1>
         {/* Active className gets applied automatically in v7 */}
         <NavLink to="/" end>
           Home
