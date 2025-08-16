6a458f1c387bf58acac633417462baf9d6f7f5a8
Batool Kazmi <kazmib6@gmail.com>
2025-08-16 16:54:23 -0400

    footer done for portal

--- frontend/cinematch/src/components/Footer.tsx
+++ frontend/cinematch/src/components/Footer.tsx
@@ -1,5 +1,31 @@
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SwipeIcon from "@mui/icons-material/Swipe";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import PersonIcon from "@mui/icons-material/Person";
 
-export default Footer;
+export default function Footer() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <div className="border-t border-gray-300 bg-white">
+      <Box>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Chats" icon={<ChatBubbleIcon />} />
+          <BottomNavigationAction label="Matches" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Swipe" icon={<SwipeIcon />} />
+          <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+        </BottomNavigation>
+      </Box>
+    </div>
+  );
+}
